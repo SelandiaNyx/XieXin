a80d4208b98fb099c4fa80df44d47837e8478f20
SelandiaNyx <wenkail264@gmail.com>
2026-09-22 05:06:43 +0800

    Keep portable release only

--- packaging/install.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set "APP_DIR=%LOCALAPPDATA%\Programs\XieXin"
-if not exist "%APP_DIR%" mkdir "%APP_DIR%"
-copy /Y "%~dp0XieXin.exe" "%APP_DIR%\XieXin.exe" >nul
-copy /Y "%~dp0WebView2Loader.dll" "%APP_DIR%\WebView2Loader.dll" >nul
-start "XieXin" "%APP_DIR%\XieXin.exe"
-exit /b 0
